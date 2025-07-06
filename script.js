30580c0684c7450c791b86d9590e807101cb6de7
jhoncavalli <jhoncavalli@hotmail.com>
2025-07-06 13:51:29 -0300

    corrige a referência da variável btnBack e adiciona a lógica de clique para os botões de navegação

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 let btnNext = document.querySelector('.next')
-let btnback = document.querySelector('.back')
+let btnBack = document.querySelector('.back')
 
 let container = document.querySelector('.container')
 let list = document.querySelector('.container .list')
 let thumb = document.querySelector('.container .thumb')
 
+btnNext.onclick = () => moveItemOnClick('next')
+btnBack.onclick = () => moveItemOnClick('back')
 
-function 
+function moveItemOnClick(type) {
+    let listItems = document.querySelectorAll('.list .list-item')
+    let thumbItems = document.querySelectorAll('.thumb .thumb-item')
+
+
+    if(type === 'next') {
+        list.appendChild(listItems[0])
+        thumb.appendChild(thumbItems[0])
+    } else {
+        list.prepend(listItems[listItems.length - 1])
+        thumb.prepend(thumbItems[listItems.length - 1])
+        
+    }
+}
