5357309579a1d421fe98886837f0c1002ccf0192
jhoncavalli <jhoncavalli@hotmail.com>
2025-07-06 13:55:09 -0300

    adiciona lógica para adicionar e remover classes de animação nos botões de navegação

--- script.js
+++ script.js
@@ -16,9 +16,18 @@ function moveItemOnClick(type) {
     if(type === 'next') {
         list.appendChild(listItems[0])
         thumb.appendChild(thumbItems[0])
+        container.classList.add('next')
     } else {
         list.prepend(listItems[listItems.length - 1])
         thumb.prepend(thumbItems[listItems.length - 1])
+        container.classList.add('back')
         
     }
+
+    setTimeout(() => {
+        container.classList.remove('next')
+        container.classList.remove('back')
+    }, 3000);
+
+
 }
