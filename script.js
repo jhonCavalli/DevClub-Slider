2ff0519ab921b48ab652f54501df8f9b157f8b94
jhoncavalli <jhoncavalli@hotmail.com>
2025-07-06 13:28:37 -0300

    adiciona variáveis para os botões de navegação e elementos do slider

---
+++ script.js
@@ -0,0 +1,9 @@
+let btnNext = document.querySelector('.next')
+let btnback = document.querySelector('.back')
+
+let container = document.querySelector('.container')
+let list = document.querySelector('.container .list')
+let thumb = document.querySelector('.container .thumb')
+
+
+function 
